0d9fb172d52817f9f5895f34392abbc37c2338c8
GabrielR4SH <bieelsantana23@hotmail.com>
2024-05-29 15:29:45 -0300

    Yup Validation datas in Controller

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,21 +1,32 @@
 import {Request, Response} from 'express';
+import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
 
 interface ICidade{
   nome: string;
 }
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
+//Yup validation 
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+});
+
+
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+  let validatedData: ICidade| undefined = undefined;
+
+  try {
+    validatedData = await bodyValidation.validate(req.body);
+    return res.status(StatusCodes.ACCEPTED).json('Created Done');
+  }catch( error){
   
+    const yupError = error as yup.ValidationError;
 
-  console.log(req.body);
-
-
-
-
-
-
-return res.send('Created');
-
-
-
+    return res.json({
+      errors: {
+        default: yupError.message,
+      }
+    });
+  }
 };
+
